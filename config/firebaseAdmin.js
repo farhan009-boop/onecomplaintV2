01061a60d23cab695c5a28ddadb806203a34058c
Mohd Farhan <mohdfarhan.com109@gmail.com>
2026-07-02 21:54:59 +0530

    Support Firebase service account via environment variable

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,10 +1,18 @@
 console.log("firebaseAdmin.js loaded");
-const admin = require("firebase-admin");
-const path = require("path");
 
-const serviceAccount = require(path.join(__dirname, "firebase-service-account.json"));
+const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
+  let serviceAccount;
+
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Render: read from environment variable
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else {
+    // Local: read from JSON file
+    serviceAccount = require("./firebase-service-account.json");
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
